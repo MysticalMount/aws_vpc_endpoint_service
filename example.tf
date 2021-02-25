c12f97cdaa18661cf79b94c393da6584d90122e5
James Wilson <jay.wilson@capgemini.com>
2021-02-25 15:40:40 +0000

    Initial commit.

---
+++ example.tf
@@ -0,0 +1,13 @@
+data "external" "endpoint_services" {
+  program = ["bash", "./custom_scripts/get_endpoint_service.sh"]
+
+  query = {
+    vpce_region  = "eu-west-2"
+    vpce_service = "s3"
+    vpce_type    = "Gateway"
+  }
+}
+
+output "result" {
+  value = "${data.external.endpoint_services.result.endpoint}"
+}
